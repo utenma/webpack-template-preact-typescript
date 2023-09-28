65101cf91cf59e1a63379276c88ed8f879bd2048
Sal Pal <9655605-utenma@users.noreply.gitlab.com>
2023-09-28 16:36:01 -0700

    App: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import type { FC } from "react";
 import { counter } from "./counter";
+import { default as Logo } from './logo.svg'
 
 export const App: FC = () => {
 
   return (
     <div
       css={{
+        paddingTop: 16,
         display: 'flex',
         flexDirection: 'column',
-        gap: 8
+        gap: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <span>
-        Hello World {counter.value}
+        Hello World
+      </span>
+      <Logo
+        css={{
+          width: 48,
+          height: 48,
+          transition: 'rotate 1s',
+          'div:hover > &': {
+            rotate: '180deg'
+          }
+        }}
+      />
+      <span>
+        {counter.value}
       </span>
       <button
         css={{
           borderWidth: 1,
           borderStyle: 'solid',
-          borderColor: 'gray',
+          borderColor: '#777',
           background: 'transparent',
+          borderRadius: 8,
+          padding: 4,
           ':hover': {
-            color: 'white',
-            backgroundColor: 'gray',
-            borderColor: 'gray'
+            color: '#fff',
+            backgroundColor: '#777',
+            borderColor: '#777',
           }
         }}
         onClick={() => counter.value++}
